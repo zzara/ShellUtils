1460cc4004573c782721660f125e8450c532c78f
zzara <36560280+zzara@users.noreply.github.com>
2018-07-28 10:20:02 -0700

    Update sortArray.sh

--- sortArray.sh
+++ sortArray.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # sort elements of an array using bidirectional method
-#array=(0 4 6 8 2 9 5 7 1 3)
 array=(1.36 0.53 0.98 1.92 3.18 -0.12)
 size=${#array[@]}
-for (( i = 0; i < ${size}; i++ )); do
+for (( i = 0; i < size; i++ )); do
 	# sort
-	for (( j = i + 1; j < ${size}; j++ )); do
+	for (( j = i + 1; j < size; j++ )); do
 		if [[ ${array[${i}]} > ${array[${j}]} ]]; then
 			a=${array[${i}]}
 			array[${i}]=${array[${j}]}
