f14f923e91b908277475bd18f403a76fbe1cad72
zzara <36560280+zzara@users.noreply.github.com>
2018-07-28 09:09:22 -0700

    Update sortArray.sh

--- sortArray.sh
+++ sortArray.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# sorts elements of an integer array into ascending order
+# sort elements of an array
+#array=(79 82 32 190 254 6 197 107 191 131 84 62 179 100 158 127 92)
 array=(1.36 0.53 0.98 1.92 3.18 -0.12)
 size=${#array[@]}; n=1; y=$(($n+1))
 for (( i = 0; i < ${size}; i++ )); do
@@ -13,4 +14,3 @@ for (( i = 0; i < ${size}; i++ )); do
 done
 echo "All elements of the array have been sorted:"
 echo "${array[*]}"
-exit 0
