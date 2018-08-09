1d83ad8a93a5c300d01d46df4e06e9e726642d97
zzara <36560280+zzara@users.noreply.github.com>
2018-08-09 08:34:38 -0700

    Update primeFinder.c

--- primeFinder.c
+++ primeFinder.c
@@ -4,15 +4,27 @@
 // infinitely check for prime numbers in probably the least efficient way :D
 int main (void){
     int i,j;
-    for (i=2;i>0;i++){
-        for (j=2;j<=i;j++){
-            if (i % j == 0 && j != i){
+    for (i=1;i<20;i++){
+        // error checking -- fails if the starting number is not greater than or equal to 1
+        if (i >=1 ){
+            if (i == 1){
+                printf("1 is not a prime number\n");
                 break;
             }
-            if (i % j == 0 && j == i){
-                printf("%d is prime\n",i);
-                break;
+            for (j=2;j<=i;j++){
+                // if the target number is divisible by any other number pther than itself, break the loop and continue to check the next target number
+                if (i % j == 0 && j != i){
+                    break;
+                }
+                // if the target number is only divisible by itself, print the result
+                if (i % j == 0 && j == i){
+                    printf("%d is prime\n",i);
+                    break;
+                }
             }
+        }
+        else{
+            printf("'i' is not greater than or equal to 1. Fix your starting number and run this program again.\n");
         }
     }
 return EXIT_SUCCESS;
