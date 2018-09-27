d4512bdfc9689d132fbfdbc7ebd1bb94143e8b23
zzara <36560280+zzara@users.noreply.github.com>
2018-09-27 13:13:15 -0700

    Update base64multi.sh

--- base64multi.sh
+++ base64multi.sh
@@ -3,31 +3,25 @@
 # usage:
 # bash base64multi.sh encode 3 -i ~/Desktop/file.txt -o ~/Desktop/file2.txt
 # bash base64multi.sh decode 3 -i ~/Desktop/file.txt -o ~/Desktop/file2.txt
+if [[ ${3} == "-i" ]]; then
+    flagz=$( cat ${4} )
+else
+    flagz="${3}"
+fi
 case $1 in 
     encode )
-        if [[ ${3} == "-i" ]]; then
-            flagz=$( cat ${4} )
-        else
-            flagz="${3}"
-        fi
         for i in `seq 1 ${2}`; do
             flagz=$( echo "${flagz}" | base64 )
         done
-        echo "${flagz}"
         ;;
     decode )
-        if [[ ${3} == "-i" ]]; then
-            flagz=$( cat ${4} )
-        else
-            flagz="${3}"
-        fi
         for i in `seq 1 ${2}`; do
             flagz=$( echo "${flagz}" | base64 -D)
         done
-        echo "${flagz}"
         ;;
     *) echo "Invalid option  -- takes 'encode' or 'decode' as first arg"; exit;;
 esac
+echo "${flagz}"
 if [[ $4 == "-o" ]]; then
     echo "${flagz}" >>${5}
 elif [[ $5 == "-o" ]]; then
