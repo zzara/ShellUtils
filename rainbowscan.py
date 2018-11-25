54557b5e7cfe73bf5f13d92b0a630e55e6cfb4bf
zzara <36560280+zzara@users.noreply.github.com>
2018-11-25 12:11:05 -0700

    Update rainbowscan.py

--- rainbowscan.py
+++ rainbowscan.py
@@ -12,36 +12,47 @@ from scapy.layers.inet import IP, TCP, ICMP
 from scapy.sendrecv import send, sr
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--host', action="store")
+parser.add_argument('-k', '--host', action="store")
+parser.add_argument('-l', '--level', action="store")
 parser.add_argument('-u', '--udp', action="store_true")
 parser.add_argument('-s', '--syn', action="store_true")
 parser.add_argument('-t', '--tcp', action="store_true")
 parser.add_argument('-i', '--icmp', action="store_true")
-parser.add_argument('-r', '--rainbow', action="store_true")
-
 args = parser.parse_args()
 
+if args.level == 'low' or args.level is None:
+    level = 20
+
+elif args.level == 'med':
+    level = 100
+
+elif args.level == 'low':
+    level = 500
+
+elif args.level == 'insane':
+    level = 1000
+
 print('scanning the crap out of {}...'.format(sys.argv[1]))
-for _ in range(1,1000):
+for port in range(1,level):
     if args.icmp:
         # icmp scan
         network_layer = IP(dst=args.host)
         sr(network_layer/ICMP(), retry=0, timeout=0.001, verbose=False)
-        print('syn packet sent to port {}'.format(_))
+        print('syn packet sent to port {}'.format(port))
 
     if args.syn:
         # synscan
         network_layer = IP(dst=args.host)
-        transport_layer = TCP(dport=_, flags="S")
+        transport_layer = TCP(dport=port, flags="S")
         send(network_layer/transport_layer, verbose=False)
-        print('syn packet sent to port {}'.format(_))
+        print('syn packet sent to port {}'.format(port))
 
     if args.udp:
         # udp scan
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.settimeout(.01)
-        result = sock.connect_ex((args.host, _))
-        print('connecting to port {}'.format(_))
+        sock.sendto(bytes('rainbows', "utf-8"), (args.host, port))
+        print('sending to port {}'.format(port))
         sock.shutdown(socket.SHUT_RDWR)
         sock.close()
 
@@ -49,7 +60,7 @@ for _ in range(1,1000):
         # tcp scan
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(.01)
-        result = sock.connect_ex((args.host, _))
-        print('connecting to port {}'.format(_))
+        result = sock.connect_ex((args.host, port))
+        print('connecting to port {}'.format(port))
         sock.shutdown(socket.SHUT_RDWR)
         sock.close()
